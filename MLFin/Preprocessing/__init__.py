a7b7ebd83523b2b17bfd4b4c71807a9cf386ad2a
boneil33 <53190565+boneil33@users.noreply.github.com>
2020-02-08 17:10:45 -0800

    remove MLFin reference

--- MLFin/Preprocessing/__init__.py
+++ MLFin/Preprocessing/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from MLFin.Preprocessing.labeling import *
-from MLFin.Preprocessing.sampling import *
-from MLFin.Preprocessing.etf_trick import ETFTrick
-from MLFin.Preprocessing.feature_importance import *
+from Preprocessing.labeling import *
+from Preprocessing.sampling import *
+from Preprocessing.etf_trick import ETFTrick
+from Preprocessing.feature_importance import *
