68ae40dc0a90cb4291e1fd644aef7c7fd63e3138
boneil33 <53190565+boneil33@users.noreply.github.com>
2020-11-03 09:51:37 -0800

    update signals
    add cusum_sym_filter, signal response plots, fix bugs on exposures and pnl (-= 1 at the entry index rather than = 0), acorr adjusted monthly freq sharpes, and likely more bugfixes ,

--- MLFin/Research/signals.py
+++ MLFin/Research/signals.py
@@ -15,7 +15,7 @@ from Preprocessing.sampling import get_num_conc_events_side, get_max_concurrency
 from Preprocessing.etf_trick import ETFTrick
 
 
-def lookback_zscore(close, lookback, vol_lookback, log_ret=True, center=True):
+def lookback_zscore(close, lookback, vol_lookback, log_ret=True, center=True, vs_ma=False):
     """
     Generate lookback-period return zscores
     
@@ -26,8 +26,11 @@ def lookback_zscore(close, lookback, vol_lookback, log_ret=True, center=True):
     """
     if log_ret:
         close = close.apply(np.log)
-
-    rets = close.diff(lookback).dropna()
+    
+    if vs_ma:
+        rets = close.subtract(close.rolling(lookback).mean()).dropna()
+    else:
+        rets = close.diff(lookback).dropna()
     
     if center:
         rets = rets.subtract(rets.rolling(vol_lookback).mean())
@@ -52,6 +55,36 @@ def zscore_signal(zscores, threshold, signal_type='Reversion'):
     return signals
 
 
+def cusum_sym_filter(srs, threshold_pct):
+    """
+    Symmetric cusum filter, adapted to trigger when series is over threshold but has a one day reversal
+    :param srs: (pd.Series) some series of values
+    :param threshold: (float) percentage of series mean at which to trigger signal
+    :returns: (pd.Series) signed signals indexed by trigger date
+    """
+    dates = []
+    signals = []
+    thresh = srs.mean()*threshold_pct
+    diffs = srs.diff(1).dropna()
+    sPlus = 0
+    sMinus = 0
+    for idx in diffs.index[1:]:
+        val = diffs.loc[idx]
+        
+        sPlus = max(0, sPlus+val)
+        sMinus = min(0, sMinus+val)
+        if val < 0:# and sPlus > thresh:
+            sPlus = 0
+            dates.append(idx)
+            signals.append(-1)#srs.loc[idx])
+        elif val > 0:# and sMinus < -thresh:
+            sMinus = 0
+            dates.append(idx)
+            signals.append(1)#srs.loc[idx])
+    
+    return pd.Series(signals, index=dates, name='CusumFilter')
+
+
 def get_sizes(events, discrete_width, prob, pred=None, num_classes=2):
     """
         Determine bet sizes from predicted probabilities
@@ -64,7 +97,8 @@ def get_sizes(events, discrete_width, prob, pred=None, num_classes=2):
     :return: (pd.Series) bet sizes for each event in [-1.0, 1.0]
     """
 def zscore_sizing(signals, close, vertbar, lookback=1, vol_lookback=100, pt_sl=(1,1), 
-                  trgt=None, model_resids=None, max_signals=None, even_weight=False):
+                  trgt=None, model_resids=None, max_signals=None, even_weight=False,
+                 exit_pct=False):
     """
         Generate events dataframe with sizes. 
         Size is the trade size, each signal being a different trade.
@@ -83,7 +117,7 @@ def zscore_sizing(signals, close, vertbar, lookback=1, vol_lookback=100, pt_sl=(
         trgt = get_lookback_vol(close, lookback, volwindow=vol_lookback,ewma=False)
     events0 = t1.rename('t1').to_frame().merge(filtered_signals.rename('side'),
                         left_index=True, right_index=True)
-    events, df0 = get_events(events0, close, trgt, pt_sl=pt_sl,model_resids=model_resids)
+    events, df0 = get_events(events0, close, trgt, pt_sl=pt_sl, model_resids=model_resids, exit_pct=exit_pct)
     
     # generate sizing based on inverse concurrency
     if len(events)==0:
@@ -101,9 +135,8 @@ def zscore_sizing(signals, close, vertbar, lookback=1, vol_lookback=100, pt_sl=(
                                           ), axis=1)
     
     # if we've never had that many concurrent trades, size is 0.1
-    # handle unnamed index
     if events.index.name is not None:
-        events_index_name = events.index.name
+        events_index_name = events.index.name 
     else:
         events.index.name = 'Date'
         events_index_name = 'Date'
@@ -116,10 +149,11 @@ def zscore_sizing(signals, close, vertbar, lookback=1, vol_lookback=100, pt_sl=(
     # if max_signals, drop consecutive trades after that number of signals
     if max_signals is not None:
         events = events[~((events['long']>max_signals) | (events['short']>max_signals))]
+        
     
     events.set_index(events_index_name, inplace=True)
     
-    return events
+    return events, df0
 
 
 def generate_pnl(events, close_tr, pct_change=True):
@@ -166,7 +200,8 @@ def generate_mtm_pnl(events, close_tr, log_diff=True, tc=0.0):
     pnl_df = pd.Series(0, index=close_tr.index, name='strat')
     for row in events.itertuples():
         pnl_df.loc[row.Index:row.t1] += close_diff.loc[row.Index:row.t1]*row.side*row.size#*row.trgt
-        pnl_df.loc[row.Index] = -tc # we open position at the close, should be no pnl
+        # we open position at the close, should be no pnl contribution, just the TC
+        pnl_df.loc[row.Index] += -tc - close_diff.loc[row.Index]*row.side*row.size
         pnl_df.loc[row.t1] += -tc
     
     #re-exponentiate
@@ -184,7 +219,7 @@ def generate_exposures(events, close):
     exposures = pd.Series(0, index=close.index)
     for row in events.itertuples():
         exposures.loc[row.Index:row.t1] += row.side*row.size
-        exposures.loc[row.Index] = 0
+        exposures.loc[row.Index] -= 1
     
     return exposures
 
@@ -206,6 +241,34 @@ def generate_pnl_index(mtm_pnl, rebal_cost=None):
     trick = ETFTrick(df1.shift(1), df1, df1, carry_of_strat)
     index_pnl = trick.get_etf_series()
     return index_pnl
+
+
+def createPerfSummaryDFs(summaries):
+    idx = pd.IndexSlice
+    summaries_df = pd.DataFrame(summaries).T
+    summaries_df.index.set_names(['max_signals', 'resid_lookback', 'entry_z', 'exit_z'], inplace=True)
+    summaries_df = summaries_df.reset_index()
+    summaries_df.loc[:, 'Avg. Trade Days'] = summaries_df.loc[:, 'Avg. Trade Days'].dt.days
+    #summaries_df.loc[:, ['entry_z', 'exit_z']] = summaries_df.loc[:, ['entry_z', 'exit_z']]
+    summaries_df.set_index(['max_signals', 'resid_lookback', 'entry_z', 'exit_z'], inplace=True)
+    summaries_df_noindex = summaries_df.reset_index()
+    conv_cols = summaries_df_noindex.select_dtypes(include='object').columns
+    summaries_df_noindex.loc[:, conv_cols] = summaries_df_noindex.loc[:, conv_cols].astype(float, errors='ignore')
+    return summaries_df, summaries_df_noindex
+
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+def get_event_bins(event_row, bins):
+    score = event_row['entry_score']
+    if score < bins[0]:
+        return 0
+    elif score > bins[-1]:
+        return len(bins)
+    else:
+        for i in range(len(bins)-1):
+            if score >= bins[i] and score < bins[i+1] :
+                return i+1
+    raise ValueError('Did not fall in a bin')
 
 
 def _get_tc(events, tc_pct, rebal_cost):
@@ -251,14 +314,22 @@ def generate_perf_summary(events, close_tr, tc_pct=0.0, rebal_cost=None):
         pnl_index = generate_pnl_index(pnl)
         
     last_date = min(np.hstack((events['t1'].iloc[-1],pnl_index.index[-1])))
+    pnl_index = pnl_index.loc[events.index[0]:last_date]
     years_live = (last_date-events.index[0]).days/365.25
     cagr = np.power(pnl_index.iloc[-1]/pnl_index.iloc[0],1/years_live)-1.
     
     log_returns = pnl_index.apply(np.log).diff(1).dropna()
-    vol = log_returns.std()
+    log_m_returns = pnl_index.copy(deep=True).asfreq('BM').apply(np.log).diff(1).dropna()
+    vol_d = log_returns.std()
+    vol_m = log_m_returns.std()
     # assumes daily close data
-    annualized_vol = vol*np.sqrt(252)
-    sharpe = cagr/annualized_vol
+    annualized_dvol = vol_d*np.sqrt(252)
+    sharpe_d = cagr/annualized_dvol
+    # monthly and account for autocorr
+    acf = tsa.stattools.acf(log_m_returns, nlags=11)
+    annualized_mvol = vol_m*np.sqrt(12+2*sum([(12-i)*acf[i] for i in range(1,12)]))
+    sharpe_m = cagr/annualized_mvol
+    
     drawdown_pct = pnl_index.divide(pnl_index.expanding(0).max())-1.
     max_dd = np.min(drawdown_pct)
     calmar = -cagr/max_dd
@@ -271,15 +342,56 @@ def generate_perf_summary(events, close_tr, tc_pct=0.0, rebal_cost=None):
     hit_ratio = events[events['ret']>0].shape[0]/num_trades
     avg_trade_days = (events['t1']-events.index).mean()
     
-    summary = pd.Series([cagr,annualized_vol,sharpe,calmar, max_dd, num_trades, avg_pnl, 
-                         long_ratio, hit_ratio, avg_trade_days], 
-                       index=['Ann. Ret.','Ann. Vol.','Sharpe','Calmar','Max Drawdown','# Trades',
-                              'Avg. PnL', 'Long%Signals', 'Hit Ratio', 'Avg. Trade Days'])
+    summary = pd.Series([cagr, annualized_dvol, annualized_mvol, sharpe_d, sharpe_m, calmar, max_dd, num_trades, avg_pnl, 
+                         long_ratio, hit_ratio, avg_trade_days, events.index.min(), events.t1.max()], 
+                       index=['Ann. Ret.', 'Ann. Vol.', 'Ann. Vol. (m)', 'Sharpe', 'Sharpe (m)', 
+                              'Calmar', 'Max Drawdown', '# Trades',
+                              'Avg. PnL', 'Long%Signals', 'Hit Ratio', 'Avg. Trade Days', 'First Entry', 'Last Exit'])
     return summary
    
+
+def draw_signal_response(events, bstart=-2., bend=2., bwidth=0.25):
+    # weight the returns by avg uniqueness and take away side (so want shorts to return negative)
+    events['tw'] = get_events_avg_uniqueness(events['t1'], events['t1'])
+    events['wgt_ret_noside'] = -events['ret']*events['side']*events['tw']
+
+    bwidth = 0.25
+    bstart = -2.
+    bend = 2.25
+    bins = np.arange(bstart, bend, bwidth) #[-2., -1.5, -1., -.75, -.5, -.25, 0, .25, .5, .75, 1., 1.5, 2.]
+    
+    # get integer bins for signals
+    events['bin'] = events.apply(lambda x: get_event_bins(x, bins), axis=1)
+    fig, ax = plt.subplots(figsize=(8,6), dpi=300)
+    ax = sns.boxplot('bin', 'wgt_ret_noside', data=events, ax=ax, notch=True)
+
+    ticks = ax.get_xticks()
+    ax.set_xticks(ticks+0.5)
+    ax.set_xticklabels(bins)
+    # get index of 0
+    idx0 = bins.searchsorted(0)
+    ax.axvline(idx0+0.5, c='r', ls='-.', lw=0.5) #0 will be shifted right 0.5
+
+    divider = make_axes_locatable(ax)
+    axHist = divider.append_axes("bottom", 1.2, pad=0.1)#, sharex=ax)
+    adj_bins = bins
+    adj_bins = np.insert(adj_bins, 0, bstart-bwidth)
+    adj_bins = np.append(adj_bins, bend+bwidth)
+    axHist.hist(events['bin'], bins=range(len(bins)+2), rwidth=0.9)
+
+    axHist.set_xticks(ticks+1.0)
+    axHist.set_xticklabels(bins)
+    ax.set_xlim(-0.5, len(bins)+0.5)
+    axHist.set_xlim(0, len(bins)+1.0)
+    axHist.set_xlabel('entry_z')
+    ax.set_title('Signal Response')
+
+    plt.show()
+    
     
 def run_resid_backtest(closes, weights, carry, resids, resid_lookback_diff, vol_lookback, entry_threshold, pt_sl, vertbar,
-                       max_signals=1, even_weight=True, log_ret=False, rebal_freq=None, plot=True, tc_pct=0.0, roll_cost=None):
+                       max_signals=1, even_weight=True, log_ret=False, vs_ma=False, rebal_freq=None, plot=True, tc_pct=0.0, 
+                       roll_cost=None, cusum_pct=None, exit_pct=False, window=3.):
     """
         Run a backtest based on residual reversion. Plot the resulting positions and MTM pnl, and show the stats summary
     :param resids: (pd.Series) of residuals
@@ -301,14 +413,26 @@ def run_resid_backtest(closes, weights, carry, resids, resid_lookback_diff, vol_
 
     # rolling change zscores of the aggregated residuals
     # TODO: maybe we should consider change versus a moving average rather than a specific lookback?
-    zscore_resids = lookback_zscore(resids, lookback=resid_lookback_diff, vol_lookback=vol_lookback, log_ret=log_ret)
+    zscore_resids = lookback_zscore(resids, lookback=resid_lookback_diff, vol_lookback=vol_lookback, 
+                                    log_ret=log_ret, vs_ma=vs_ma)
     
     # generating over threshold .75 z-score signals for reversion
-    signals = zscore_signal(zscore_resids, threshold=entry_threshold, signal_type='Reversion')
+    signals = zscore_signal(zscore_resids, threshold=entry_threshold, signal_type='Reversion').rename('signal')
 
+    # cusum reversal too
+    if cusum_pct is not None:
+        cusum_signals = cusum_sym_filter(resids, cusum_pct)
+        # full signal from cusum and zscore
+        full_signals = signals.to_frame().join(cusum_signals).fillna(0).apply(lambda x: x['signal'] if x['signal']==x['CusumFilter'] 
+                                                                      else 0, axis=1)
+    else:
+        full_signals = signals
+    
     # generate initial events DataFrame for this reversion strategy
-    events0, df0 = zscore_sizing(signals, trs, vertbar=vertbar, pt_sl=pt_sl, 
-                                 max_signals=max_signals, even_weight=even_weight, model_resids=zscore_resids)
+    events0, df0 = zscore_sizing(full_signals, trs, vertbar=vertbar, pt_sl=pt_sl, 
+                                 max_signals=max_signals, even_weight=even_weight, model_resids=zscore_resids,
+                                exit_pct=exit_pct)
+    
     events = generate_pnl(events0, trs, pct_change=True)
     
     # tc cost on $1 (i.e. 1bp = 1e-4), weights_diff_delev is absolute change in weights
@@ -324,18 +448,34 @@ def run_resid_backtest(closes, weights, carry, resids, resid_lookback_diff, vol_
     # update events['ret'] for tc and calculate full cost to apply to mtm_pnl
     events, rebal_cost_strat = _get_tc(events, tc_pct, total_rebal_cost_srs['total_cost'])
     
-    mtm_pnl = generate_mtm_pnl(events, trs, tc_pct) # subtract tc from return on open and close, doesn't use ret
+    mtm_pnl = generate_mtm_pnl(events, trs, log_diff=True, tc=tc_pct) # subtract tc from return on open and close, doesn't use ret
     rebal_cost_strat = rebal_cost_strat.reindex(mtm_pnl.index, fill_value=0.0).rename('strat')
     
     pnl_index = generate_pnl_index(mtm_pnl, rebal_cost_strat)
     exposures = generate_exposures(events, trs)
     if plot:
-        fig, ax = plt.subplots(figsize=(6,6), nrows=3, dpi=300)
+        # compute data for rolling sharpe
+        window_m = int(window*12)
+        rolling_pct = pnl_index.apply(np.log).diff(252*window)
+        cagr_srs = np.power(pnl_index.pct_change(int(252*window))+1., 1/window)-1.
+
+        log_m_returns = pnl_index.copy(deep=True).asfreq('BM', method='ffill').apply(np.log).diff(1).dropna()
+
+        vol_m_srs = log_m_returns.rolling(window_m).std()
+
+        # monthly and account for autocorr
+        acorr_scale = log_m_returns.rolling(window_m).apply(lambda x: np.sqrt(12 + 
+                                                              2*sum([(12-i) * x.autocorr(lag=i) for i in range(1,12)])), 
+                                                              raw=False).fillna(1.)
+        mvol_srs_ann = vol_m_srs * acorr_scale
+        sharpe_m = cagr_srs.asfreq('BM', method='ffill') / mvol_srs_ann
+        fig, ax = plt.subplots(figsize=(6,7), nrows=3, dpi=300)
         exposures.plot(ax=ax[0])
         pnl_index.plot(ax=ax[1])
-        trs.plot(ax=ax[2])
-        ax[0].set_title("Exposure in long USDCAD vs Basket")
-        ax[1].set_title(r"Daily MTM Equity Curve ({0}bp TC)".format(int(tc_pct*1e4)))
+        sharpe_m.plot(ax=ax[2])
+        ax[0].set_title("Exposure in Basket")
+        ax[1].set_title(r"Daily MTM Equity Curve ({0}bp TC)".format(np.round(tc_pct*1e4, 1)))
+        ax[2].set_title(r"Rolling {0}yr Sharpe {1}bp TC".format(window, np.round(tc_pct*1e4, 1)))
         fig.tight_layout()
         plt.show()
     perf_summary = generate_perf_summary(events, trs, tc_pct=tc_pct, rebal_cost=rebal_cost_strat)
